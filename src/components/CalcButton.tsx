9048bcfb11fb660fc8275c2b5b8ad2fb49f303f1
raj-pat <rajp4445@gmail.com>
2020-05-12 22:38:10 -0700

    Add CSS

--- src/components/CalcButton.tsx
+++ src/components/CalcButton.tsx
@@ -2,14 +2,52 @@ import React from "react";
 
 export interface CalcButtonProps {
   innerSymbol: string;
+  type: string;
 }
 
 const CalcButton: React.SFC<CalcButtonProps> = (props) => {
+  //btn inside
+  //isSymbol or isDigit
   return (
-    <button className="calcBtn btn btn-secondary m-1 ">
+    <button className={getClasses()} style={getStyleObject()}>
       {props.innerSymbol}
     </button>
   );
+  function getClasses() {
+    if (
+      props.innerSymbol === "0" ||
+      props.innerSymbol === "." ||
+      props.innerSymbol === "="
+    ) {
+      let col: string;
+      if (props.innerSymbol === "0") {
+        col = "-6";
+      } else {
+        col = "-3";
+      }
+      return "calcBtn btn m-1 col" + col;
+    } else {
+      return "calcBtn btn m-1 col";
+    }
+  }
+  function getStyleObject() {
+    let color: string;
+    if (props.type === "function") {
+      return {
+        backgroundColor: "darkgrey",
+      };
+    } else if (props.type === "math") {
+      return {
+        backgroundColor: "orange",
+        color: "white",
+      };
+    } else {
+      return {
+        backgroundColor: "grey",
+        color: "white",
+      };
+    }
+  }
 };
 
 export default CalcButton;
