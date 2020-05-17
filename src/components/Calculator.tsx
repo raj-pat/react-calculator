22e4f647ac8fbd01954f8c81351c472a34c85b88
raj-pat <rajp4445@gmail.com>
2020-05-16 22:15:07 -0700

    convert sfc to cc

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -3,55 +3,117 @@ import Screen from "./Screen";
 import Buttons from "./Buttons";
 import { onButtonClickInterface } from "../Interfaces/functions";
 
-const Calculator: React.SFC<{}> = () => {
-  const [output, setOutput] = useState("0");
-  const [isFloat, setIsFloat] = useState(false);
-  const [previous, setPrevious] = useState("");
-  const [currMath, setCurrMath] = useState("");
-  const [resetScreenNext, setResetScreenNext] = useState(false);
-  const operandLimit = 10;
+interface CalculatorStateInterface {
+  output: string;
+  isFloat: boolean;
+  previous: string;
+  currMath: string;
+  resetScreenNext: boolean;
+}
 
-  let onButtonClick: onButtonClickInterface = (innerSymbol, type) => {
-    if (output.length === operandLimit) {
-      return;
-    }
+class Calculator extends React.Component<{}, CalculatorStateInterface> {
+  static operandLimit: number = 10;
 
-    switch (type) {
-      case "digit":
-        if (resetScreenNext) {
-          setOutput("0");
-          console.log(output);
-          setResetScreenNext(false);
-        }
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      output: "0",
+      isFloat: false,
+      previous: "",
+      currMath: "",
+      resetScreenNext: false,
+    };
 
-        if (innerSymbol === ".") {
-          if (isFloat) break;
-          else setIsFloat(true);
-        }
+    this.onButtonClick = this.onButtonClick.bind(this);
+  }
 
-        if (output === "0") {
-          setOutput(innerSymbol);
-        } else {
-          setOutput(output.toString() + innerSymbol);
-        }
-        break;
-      case "compute":
-        break;
-      case "math":
-        setOutput(compute(previous, currMath, output));
-        setPrevious(output);
-        setResetScreenNext(true);
-        setCurrMath(innerSymbol);
-        break;
-    }
-  };
-  return (
-    <div className="container bg-dark calcOuter">
-      <Screen screenOutput={output} />
-      <Buttons onButtonClick={onButtonClick} />
-    </div>
-  );
-};
+  onButtonClick(innerSymbol: string, type: string): void {
+    this.setState((state, props) => {
+      let newState = { ...state };
+
+      if (newState.output.length === Calculator.operandLimit) {
+        return;
+      }
+
+      switch (type) {
+        case "digit":
+          if (innerSymbol === ".") {
+            if (newState.isFloat) break;
+            else newState.isFloat = true;
+          }
+
+          if (newState.output === "0" || newState.resetScreenNext) {
+            if (newState.resetScreenNext) {
+              newState.resetScreenNext = false;
+            }
+            newState.output = innerSymbol;
+          } else {
+            newState.output = newState.output.toString() + innerSymbol;
+          }
+
+          break;
+        case "math":
+          const newOutput = compute(
+            newState.previous,
+            newState.currMath,
+            newState.output
+          );
+          newState.previous = newOutput;
+          newState.resetScreenNext = true;
+          newState.currMath = innerSymbol;
+          newState.output = newOutput;
+          break;
+        case "compute":
+          newState.output = compute(
+            newState.previous,
+            newState.currMath,
+            newState.output
+          );
+          break;
+      }
+      return newState;
+    });
+  }
+
+  render() {
+    return (
+      <div className="container bg-dark calcOuter">
+        <Screen screenOutput={this.state.output} />
+        <Buttons onButtonClick={this.onButtonClick} />
+      </div>
+    );
+  }
+
+  // let onButtonClick: onButtonClickInterface = (innerSymbol, type) => {
+
+  //
+
+  //   switch (type) {
+  //     case "digit":
+
+  //       if (innerSymbol === ".") {
+  //         if (isFloat) break;
+  //         else isFloat = true;
+  //       }
+
+  //       if (newOutput === "0" || resetScreenNext) {
+  //         if (resetScreenNext) {
+  //           resetScreenNext = false;
+  //         }
+  //         setOutput(innerSymbol);
+  //       } else {
+  //         setOutput(output.toString() + innerSymbol);
+  //       }
+  //       break;
+  //     case "compute":
+  //       break;
+  //     case "math":
+  //       console.log(previous);
+  //
+  //       break;
+  //   }
+  // };
+}
 
 const compute = (operand1: string, math: string, operand2: string): string => {
   switch (math) {
