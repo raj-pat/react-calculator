b23b1e42ff485bafb486989298fb59aa3b992772
raj-pat <rajp4445@gmail.com>
2020-05-18 16:30:48 -0700

    add history component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Calculator from "./Calculator";
 import RouteButtons from "./RouteButtons";
+import History from "./History";
 
 export interface AppProps {}
 
 const App: React.SFC<AppProps> = () => {
+  
   return (
     <div className="row justify-content-center">
       <div className="col" style={{ padding: "0" }}>
@@ -14,7 +16,7 @@ const App: React.SFC<AppProps> = () => {
             <RouteButtons />
             <Switch>
               <Route path="/history">
-                <h1>history here</h1>
+                <History content={[{ history: "aba" }]} />
               </Route>
               <Route path="/">
                 <Calculator />
